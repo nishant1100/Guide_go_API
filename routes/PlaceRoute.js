9dc0be8ea45187c4a1212bb98f7c7e3e894820a4
nishant1100 <nishantstha1010@gmail.com>
2025-02-21 15:04:28 +0545

    PlaceRoute updated + images uploaded

--- routes/PlaceRoute.js
+++ routes/PlaceRoute.js
@@ -1,17 +1,27 @@
-const express=require("express");
-const router=express.Router();
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
 const { findall, save, findById, deleteById, update } = require("../controller/PlaceController");
-const {authenticateToken,}=require("../security/Auth");
+const { authenticateToken } = require("../security/Auth");
 
+// Configure multer
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, "place_images/"); 
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + "-" + file.originalname);
+    },
+});
 
+const upload = multer({ storage: storage });
 
-
-
+// Define routes
 router.get("/", findall);
-router.get("/:id",authenticateToken,findById);
-router.delete("/:id",authenticateToken,deleteById);
-router.put("/:id",authenticateToken,update)
+router.get("/:id", findById);
+router.delete("/:id", deleteById);
+router.put("/:id", update);
+router.post("/save", upload.single("file"), save); 
 
+module.exports = router;
 
-
-module.exports=router;
